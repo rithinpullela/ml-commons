dc7fa7412f8dad08656c5be2f350149dac17bb72
rithin-pullela-aws <rithinp@amazon.com>
2025-09-24 17:07:50 -0700

    improvements
    Signed-off-by: rithin-pullela-aws <rithinp@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/tools/QueryPlanningTool.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/tools/QueryPlanningTool.java
@@ -88,7 +88,7 @@ public class QueryPlanningTool implements WithModelTool {
         + "Provide a 'question' parameter containing the complete natural language query with all necessary context, requirements, filters, and constraints."
         + "The question should be self-contained with all information needed to generate the OpenSearch DSL."
         + "Provide 'index_name' to help generate more accurate queries based on the index structure."
-        + "Optinally provide model ID to be potentially used for neural search "
+        + "Optionally provide embedding model ID to be used for neural search "
         + "The tool will return a valid OpenSearch query that can be used to search your data.";
 
     public static final String DEFAULT_INPUT_SCHEMA = "{"
@@ -96,7 +96,7 @@ public class QueryPlanningTool implements WithModelTool {
         + "\"properties\":{"
         + "\"question\":{\"type\":\"string\",\"description\":\"Complete natural language query with all necessary context to generate OpenSearch DSL. Include the question, any specific requirements, filters, or constraints. Examples: 'Find all products with price greater than 100 dollars', 'Show me documents about machine learning published in 2023', 'Search for users with status active and age between 25 and 35'\"},"
         + "\"index_name\":{\"type\":\"string\",\"description\":\"the name of the index against which the query needs to be generated.\"},"
-        + "\"neural_model_id\":{\"type\":\"string\",\"description\":\"the model id to perform neural search.\"}"
+        + "\"embedding_model_id\":{\"type\":\"string\",\"description\":\"the model id to perform neural search.\"}"
         + "},"
         + "\"required\":[\"question\", \"index_name\"],"
         + "\"additionalProperties\":false"
@@ -254,7 +254,15 @@ public class QueryPlanningTool implements WithModelTool {
                 // Return only the document content (_source field) as JSON string
                 Map<String, Object> sourceMap = hits[0].getSourceAsMap();
                 if (sourceMap != null && !sourceMap.isEmpty()) {
-                    return gson.toJson(sourceMap);
+                    Map<String, Object> truncatedSourceMap = new HashMap<>();
+                    for (Map.Entry<String, Object> entry : sourceMap.entrySet()) {
+                        if (entry.getValue().toString().length() > 100) {
+                            truncatedSourceMap.put(entry.getKey(), "[truncated]" + entry.getValue().toString().substring(0, 100));
+                        } else {
+                            truncatedSourceMap.put(entry.getKey(), entry.getValue());
+                        }
+                    }
+                    return gson.toJson(truncatedSourceMap);
                 }
             }
 
